718d655ae8a0a32eea8611ec0e16c4605ee0b592
Dmytro Artemiev <dartemiev@e1r7p10.unit.ua>
2020-11-09 18:24:58 +0200

    added header, slider

---
+++ js/script.js
@@ -0,0 +1,38 @@
+'use strict'
+
+// -----SLIDER-----
+
+let position = 0;
+let sliderTrack = document.getElementsByClassName('slider__track')[0];
+let btnPrev = document.getElementsByClassName("prev")[0];
+let btnNext = document.getElementsByClassName("next")[0];
+let contentCount = sliderTrack.children.length;
+let sliderContainer = document.getElementsByClassName('slider__container')[0];
+
+
+function nextSlide (){
+    let sliderContainerWidth = sliderContainer.clientWidth;
+    if (position === contentCount-1){
+        position = 0;
+        sliderTrack.style.transform = 'translateX(-' + sliderContainerWidth * position + 'px)';
+    }
+    else  {
+        position++;
+        sliderTrack.style.transform = 'translateX(-' + sliderContainerWidth * position + 'px)';
+    }
+}
+
+function prevSlide(){
+    let sliderContainerWidth = sliderContainer.clientWidth;
+
+    if(position === 0){
+        position = contentCount-1;
+        sliderTrack.style.transform = 'translateX(-' + sliderContainerWidth * position + 'px)';
+
+    }else{
+        position--;
+        sliderTrack.style.transform = 'translateX(-' + sliderContainerWidth * position + 'px)';
+        
+
+    }
+}
